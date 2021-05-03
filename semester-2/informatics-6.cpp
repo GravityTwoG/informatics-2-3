45e619a3b314a7aaad16a9efdc7587b84bc5b38e
GravityTwog <crytekov@gmail.com>
2021-05-03 20:29:28 +0700

    feat: printTree

--- semester-2/informatics-6.cpp
+++ semester-2/informatics-6.cpp
@@ -1,62 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Vertex {
+struct TreeNode {
   int values[2];
-  Vertex* left = NULL;
-  Vertex* right = NULL;
+  TreeNode* left = NULL;
+  TreeNode* right = NULL;
 };
 
-void printVertex(Vertex *vertex) {
-  printf("%d  %d", vertex->values[0], vertex->values[1]);
+void printTreeNode(TreeNode *treeNode) {
+  printf("[%-2d %2d]", treeNode->values[0], treeNode->values[1]);
 }
 
-void printTree(Vertex *root) {
+void printTabs(int n) {
+  for (int i = 0; i < 2*n-1; i++) {
+    printf("       ");
+  }
+}
+
+void printTree(TreeNode *root, int level) {
   char hasLeft = 0;
   char hasRight = 0;
-  if (root->left != NULL) hasLeft = 1;
-  if (root->right != NULL) hasRight = 1;
-  
-  printf("   ");
-  printVertex(root);
-  
-  if (hasLeft && hasRight) {
-    printf("\n    /\\");
-    printf("\n   /  \\\n");
-    printVertex(root->left);
-    printf("   ");
-    printVertex(root->right);
+
+  // printTabs(level);
+  printTreeNode(root);
+  printf(" ----> ");
+
+  if (root->right != NULL) {
+    printTree(root->right, level+1);
   }
-  if (hasLeft && !hasRight) {
-    printf("\n    /  ");
-    printf("\n   /    \n");
-    printVertex(root->left);
-    printf("   ");
+  if (root->left != NULL) {
+    printf("\n");
+    printTabs(level+1);
+    printf(" \\---> ");
+    printTree(root->left, level+1);
+    printf("\n");
   }
-  if (!hasLeft && hasRight) {
-    printf("\n     \\");
-    printf("\n      \\\n");
-    printf("      ");
-    printVertex(root->right);
+}
+
+TreeNode* createTreeNode(int a, int b) {
+  TreeNode* treeNode = (TreeNode*)malloc(sizeof(TreeNode));
+
+  if (treeNode != NULL) {
+    treeNode->values[0] = a;
+    treeNode->values[1] = b;
+    treeNode->left = NULL;
+    treeNode->right = NULL;
   }
+
+  return treeNode;
 }
 
 // Программа должна содержать функцию  обхода дерева с выводом  его  содержимого,  функцию  добавления  вершины дерева  (ввод),  а  также  указанную  в  варианте  функцию.
 // 3.Вершина дерева содержит два целых числа и три  указателя на  поддеревья.  Данные  в  дереве  упорядочены.   Написать функцию включения нового значения в  дерево  с  сохранением упорядоченности.
 int main() {
-  Vertex root;
-  root.values[0] = 0;
-  root.values[1] = 1;
+  TreeNode* root = createTreeNode(0, 1);
 
-  Vertex branch1;
-  branch1.values[0] = 0;
-  branch1.values[1] = 0;
-  root.left = &branch1;
-  
-  Vertex branch2;
-  branch2.values[0] = 3;
-  branch2.values[1] = 5;
-  root.right = &branch2;
+  if  (root == NULL) return -1;
 
-  printTree(&root);
+  TreeNode* branch1 = createTreeNode(0, 0);
+  root->left = branch1;
+  TreeNode* branch2 = createTreeNode(3, 5);
+  root->right = branch2;
+
+  TreeNode* branch22 = createTreeNode(7, 9);
+  branch2->right = branch22;
+  TreeNode* branch222 = createTreeNode(6, 8);
+  branch2->left = branch222;
+
+  printTree(root, 0);
   return 0;
 }
